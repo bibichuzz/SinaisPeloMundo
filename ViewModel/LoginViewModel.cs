4585af105451147594c1c0e15be7703104875d36
bia <116043123+bibichuzz@users.noreply.github.com>
2026-05-21 22:14:00 -0300

    Upload 1

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SinaisPeloMundo.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+    }
+}
