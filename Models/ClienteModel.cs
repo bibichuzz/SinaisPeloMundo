3921c68ba0a1f0022b44c814edaa900d5c3956e8
bia <116043123+bibichuzz@users.noreply.github.com>
2026-05-22 19:21:23 -0300

    Update ClienteModel.cs

--- Models/ClienteModel.cs
+++ Models/ClienteModel.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+
 namespace SinaisPeloMundo.Models
 {
     public class ClienteModel
@@ -9,34 +10,34 @@ namespace SinaisPeloMundo.Models
 
         public List<PedidoModel> Pedidos { get; set; } = new List<PedidoModel>();
 
-        [Required]
-        [MaxLength(60)]
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "Nome deve ter no máximo 60 caracteres.")]
         public string Nome { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Data de nascimento é obrigatória.")]
         public DateTime DtNascimento { get; set; }
 
-        [Required]
-        [MaxLength(11)]
-        [RegularExpression(@"^\d{11}$", ErrorMessage = "Telefone deve ter 11 dígitos.")]
+        [Required(ErrorMessage = "Telefone é obrigatório.")]
+        [MaxLength(11, ErrorMessage = "Telefone deve ter 11 dígitos.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Telefone deve conter apenas números.")]
         public string Telefone { get; set; }
 
-        [Required]
-        [MaxLength(11)]
-        [RegularExpression(@"^\d{11}$", ErrorMessage = "CPF deve ter 11 dígitos.")]
+        [Required(ErrorMessage = "CPF é obrigatório.")]
+        [MaxLength(11, ErrorMessage = "CPF deve ter 11 dígitos.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "CPF deve conter apenas números.")]
         public string Cpf { get; set; }
 
-        [Required]
-        [MaxLength(60)]
+        [Required(ErrorMessage = "Endereço é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "Endereço deve ter no máximo 60 caracteres.")]
         public string Endereco { get; set; }
 
-        [Required]
-        [MaxLength(60)]
-        [EmailAddress]
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [MaxLength(60, ErrorMessage = "E-mail deve ter no máximo 60 caracteres.")]
+        [EmailAddress(ErrorMessage = "Digite um e-mail válido.")]
         public string Email { get; set; }
 
-        [Required]
-        [MaxLength(255)]
+        [Required(ErrorMessage = "Senha é obrigatória.")]
+        [MaxLength(255, ErrorMessage = "Senha muito grande.")]
         public string Senha { get; set; }
 
         public bool Admin { get; set; } = false;
